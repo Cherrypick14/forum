18d0afe0ef1d94fcb899939689e491a201dc2a1b
Cherrypick14 <cherrypicklet@gmail.com>
2025-06-16 10:07:11 +0300

    Initialize server with routing, database, and env config

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"forum/backend/route"
+	utils "forum/backend/util"
+)
+
+func main() {
+	// load environment variables
+
+	if err := utils.LoadEnvVariables(".env"); err != nil {
+		fmt.Printf("Error loading .env file :%v", err)
+	}
+
+	//  start db
+
+	utils.Init()
+	defer utils.Database.Close()
+
+	router := route.InitRoutes()
+
+	server := &http.Server{
+		Addr:         ":8000",
+		Handler:      router,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	log.Printf("Server running at: http://localhost:%s\n", "8000")
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
+}
